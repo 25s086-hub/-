56441d29ed60f52bd07e41de222d6a8ce3bcfa9b
25s086-hub <25s086@yongsan.sen.hs.kr>
2026-07-11 15:19:37 +0900

    Update APP.py

--- APP.py
+++ APP.py
@@ -13,6 +13,6 @@ question = st.text_area("소감", placeholder="소감.")
 
 if st.button("확인"):
     if agree:
-      st.success(f"{user_id}/{학년}/{반}/{level}")
-      st.success(f"{question}")
+      st.success(user_id/학년/반/level)
+      st.success(question)
         
