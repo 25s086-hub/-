8ba3142c8d14393882772b4ad1c5202ea5b2e95b
25s086-hub <25s086@yongsan.sen.hs.kr>
2026-07-11 15:17:00 +0900

    Create APP.py

---
+++ APP.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.markdown("# 앱 ui만들기")
+st.markdown("---")
+
+user_id = st.text_input("이름", placeholder="이름")
+학년 = st.radio("학년", ["1", "2", "3"], horizontal=True)
+반 = st.number_input("반 입력", min_value=1, max_value=10, value=1)
+level = st.slider("난이도", 0, 100, 50)
+score = st.slider("점수", 0, 100, 50)
+question = st.text_area("소감", placeholder="소감.")
+
+
+if st.button("확인"):
+    if agree:
+      st.success(f"{user_id}/{학년}/{반}/{level}")
+      st.success(f"{question}")
+        
